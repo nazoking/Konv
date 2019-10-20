8300de97b4650d4c65d3287d39a2b0d13e237aa9
kita <kita@team-lab.com>
2019-10-20 20:08:51 +0900

    remove warnings

--- src/test/scala/com/teamlab/scala/konv/DefaultsTest.scala
+++ src/test/scala/com/teamlab/scala/konv/DefaultsTest.scala
@@ -4,42 +4,42 @@ import utest._
 
 object DefaultsTest extends TestSuite with Defaults {
   def tests = Tests {
-    " map" - {
-      assert(implicitly[Mapper[Map[String, Int], Map[String, Long]]].map(Map("1" -> 1)) == Map("1" -> 1L))
+    test(" map") {
+      implicitly[Mapper[Map[String, Int], Map[String, Long]]].map(Map("1" -> 1)) ==> Map("1" -> 1L)
     }
-    "same class" - {
-      assert(implicitly[Mapper[A, A]].map(A()) == A())
+    test("same class") {
+      implicitly[Mapper[A, A]].map(A()) ==> A()
     }
-    " extends" - {
+    test(" extends") {
       implicitly[Mapper[Ext, Base]].map(new Ext())
     }
-    "now low" - {
+    test("now low") {
       compileError("""
     implicitly[Konv[Base, Ext]]
     """)
     }
-    "list same item" - {
-      assert(implicitly[Mapper[List[Int], Set[Int]]].map(List(1, 2)) == Set(1, 2))
+    test("list same item") {
+      implicitly[Mapper[List[Int], Set[Int]]].map(List(1, 2)) ==> Set(1, 2)
     }
-    "list othre item use impliict Konv" - {
+    test("list othre item use impliict Konv") {
       implicit val intTOString = Mapper[Int, String](_.toString)
-      assert(implicitly[Mapper[List[Int], Set[Int]]].map(List(1, 2)) == Set(1, 2))
-      assert(implicitly[Mapper[List[Int], Set[String]]].map(List(1, 2)) == Set("1", "2"))
+      implicitly[Mapper[List[Int], Set[Int]]].map(List(1, 2)) ==> Set(1, 2)
+      implicitly[Mapper[List[Int], Set[String]]].map(List(1, 2)) ==> Set("1", "2")
     }
-    "map othre item use impliict Konv" - {
+    test("map othre item use impliict Konv") {
       implicit val intTOString = Mapper[Int, String](_.toString)
       assert(
         implicitly[Mapper[Map[String, Int], Map[String, String]]]
           .map(Map("1" -> 1, "2" -> 2)) == Map("1" -> "1", "2" -> "2")
       )
     }
-    "map option use impliict Konv" - {
+    test("map option use impliict Konv") {
       implicit val intTOString = Mapper[Int, String](_.toString)
-      assert(implicitly[Mapper[Option[Int], Option[Int]]].map(Some(1)) == Some(1))
-      assert(implicitly[Mapper[Option[Int], Option[String]]].map(Some(1)) == Some("1"))
-      assert(implicitly[Mapper[Option[Int], Option[String]]].map(None) == None)
+      implicitly[Mapper[Option[Int], Option[Int]]].map(Some(1)) ==> Some(1)
+      implicitly[Mapper[Option[Int], Option[String]]].map(Some(1)) ==> Some("1")
+      implicitly[Mapper[Option[Int], Option[String]]].map(None) ==> None
     }
-    "object to Some" - {
+    test("object to Some") {
       implicit val intTOString = Mapper[Int, String](_.toString)
       assert(implicitly[Mapper[Int, Option[String]]].map(1) == Some("1"))
     }
